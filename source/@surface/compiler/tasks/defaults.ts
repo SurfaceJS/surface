e08ea6acf3e923e3646818486c278d80c088ea28
HitalloExiled <hitalloexiled@hotmail.com>
2017-08-27 15:56:55 -0300

    Refactoring some modules and update packages to version 0.1.0

---
+++ source/@surface/compiler/tasks/defaults.ts
@@ -0,0 +1,69 @@
+import Path    = require('path');
+import Webpack = require('webpack');
+
+export let surface =
+{
+    context:    './',
+    entry:      './',
+    public:     './public',
+    publicPath: './assets',
+    filename:   '[name]/[hash].js',
+    target:     'es6'
+} as Surface.Compiler.Config;
+
+export let webpack =
+{
+    devtool: '#source-map',
+    resolve:
+    {
+        extensions: ['.ts', '.js']
+    } as Webpack.NewResolve,
+    resolveLoader:
+    {
+        modules: [Path.resolve(__dirname, '../node_modules')]
+    } as Webpack.ResolveLoader,
+    module:
+    {
+        rules:
+        [
+            {
+                test: /\.(png|jpe?g|svg)$/,
+                use:
+                [
+                    {
+                        loader: 'file-loader',
+                        options: { name: '/resources/[hash].[ext]' }
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use:
+                [
+                    { loader: 'to-string-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use:
+                [
+                    {   
+                        loader: 'html-loader',
+                        options:
+                        {
+                            attrs: ['img:src', 'link:href', 'script:src'],
+                            minify: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.ts$/,
+                use: [{ loader: 'ts-loader' }]
+            },
+        ] as Array<Webpack.Rule>,
+    } as Webpack.Module
+} as Webpack.Configuration;
+
