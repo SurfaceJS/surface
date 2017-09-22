c7bc4fbcc7a216406fe7fa1a0f9828e3c2e3ddfe
HitalloExiled <hitalloexiled@hotmail.com>
2017-09-21 22:47:40 -0300

    Sync with development branch.

--- source/@surface/compiler/index.ts
+++ source/@surface/compiler/index.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 import commander = require("commander");
-import build     = require("./tasks/build");
+import Tasks     = require("./tasks");
 
 commander
-  .version('0.1.0')
+  .version('0.0.1')
   .option('-c, --config <n>', 'Configuration file path')
   .option('-e, --env    <n>', 'Enviroment mode', /^(dev|development|prod|production)$/i)
   .option('-w, --watch', 'Watch mode compilation')
   .parse(process.argv);
 
-build(commander.config, commander.env, commander.watch);
+  Tasks.build(commander.config, commander.env, commander.watch);
---
+++ source/@surface/compiler/tasks/index.ts
@@ -0,0 +1,87 @@
+import FS      = require('fs');
+import Path    = require('path');
+import RimRaf  = require('rimraf');
+import Webpack = require('webpack');
+
+import Common = require('./common');
+import Enums  = require('./enums');
+
+/**
+ * Build Surface project using provided configuration.
+ * @param configPath Path to Surface config file.
+ * @param env        Enviroment variable.
+ * @param watch      Enable watch mode.
+ */
+export function build(configPath?: string, env?: string, watch?: boolean)
+{
+    env   = Enums.Enviroment[env || 'dev'] as string;
+    watch = !!watch;
+
+    let { root, file } = resolveConfig(configPath);
+
+    let config = Common.getConfig(root, file, env);
+
+    let compiler = Webpack(config);
+
+    let statOptions: Webpack.Stats.ToStringOptions =
+    {
+        assets:   true,
+        version:  true,
+        colors:   true,
+        warnings: true,
+        errors:   true
+    };
+
+    let callback: Webpack.Compiler.Handler =
+        (error, stats) => error ? console.log(error.message) : console.log(stats.toString(statOptions));
+
+    console.log(`Starting ${watch ? 'Watch' : 'build'} using ${env} configuration.`);
+
+    RimRaf
+    (
+        config!.output!.path!,
+        () =>
+        {
+            if (watch)
+                compiler.watch({ aggregateTimeout: 500, poll: true, ignored: /node_modules/ }, callback);
+            else
+                compiler.run(callback);
+        }
+    );
+}
+
+/**
+ * Resolve Surface config.
+ * @param path Path to Surface config file.
+ */
+function resolveConfig(path?: string): { root: string, file: string }
+{
+    if (!path)
+        path = './';
+
+    path = Path.resolve(process.cwd(), path);
+
+    if (FS.lstatSync(path).isDirectory())
+    {
+        let root = path;
+        let file = 'surface.config.json'
+        if (FS.existsSync(Path.join(path, file)))
+            return { root, file };
+
+        file = 'surface.config.js'
+        if (FS.existsSync(Path.join(path, file)))
+            return { root, file };
+
+        throw new Error('Surface configuration file not found');
+    }
+    else if (FS.existsSync(path))
+    {
+        let slices = path.replace(/\\/g, '/').split('/');
+        let file   = slices.pop() as string;
+        let root   = slices.join('/');
+
+        return { root, file };
+    }
+    else
+        throw new Error('Surface configuration file not found');
+}
