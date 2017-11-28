b544d9a384dd5a059ade1e45132f0589d64a445f
patigauto <patigauto@gmail.com>
2017-11-28 16:10:45 -0200

    Restored files deleted by mistake.

---
+++ source/@surface/compiler/@types/global/index.d.ts
@@ -0,0 +1,35 @@
+declare module '*.css'
+{
+    var _: string;
+    export default  _;
+}
+
+declare module '*.html'
+{
+    var _: string;
+    export default  _;
+}
+
+declare module '*.json'
+{
+    var _: object;
+    export default  _;
+}
+
+declare module '*.sass'
+{
+    var _: string;
+    export default  _;
+}
+
+declare module '*.scss'
+{
+    var _: string;
+    export default  _;
+}
+
+declare module '*.txt'
+{
+    var _: string;
+    export default  _;
+}
---
+++ source/@surface/custom-element/@types/global/index.d.ts
@@ -0,0 +1,12 @@
+/// <reference path='../../../compiler/@types/global/index.d.ts'/>
+
+declare interface ShadyCSS
+{
+    prepareTemplate(template: HTMLTemplateElement, name: string, element?: string);
+    styleElement(element: HTMLElement);
+}
+
+declare interface Window
+{
+    ShadyCSS: ShadyCSS;
+}
