f7cadbc3d2470b5bffd01c2033026962e07facc7
Rafael Franca <rafael.franca@meta.com.br>
2018-09-17 13:00:10 -0300

    Removed proxyFactory tests.

--- source/@surface/core/tests/common/object.spec.ts
+++ source/@surface/core/tests/common/object.spec.ts
@@ -1,6 +1,6 @@
-import { shouldPass, suite, test }            from "@surface/test-suite";
-import * as chai                              from "chai";
-import { merge, objectFactory, proxyFactory } from "../../common/object";
+import { shouldPass, suite, test } from "@surface/test-suite";
+import * as chai                   from "chai";
+import { merge, objectFactory }    from "../../common/object";
 
 @suite
 export default class CommonObjectSpec
@@ -64,65 +64,5 @@ export default class CommonObjectSpec
         };
 
         chai.expect(actual).to.deep.equal(expected);
-    }
-
-    @test @shouldPass
-    public proxyFactory(): void
-    {
-        const signature = { foo: 1, bar: "", baz: { foobar: false } };
-
-        const proxy = proxyFactory(signature);
-
-        const data      = { foo: 1, bar: "bar", baz: { foobar: false } };
-        const proxyData = new proxy(data);
-
-        const descriptors = Object.getOwnPropertyDescriptors(proxy.prototype);
-
-        chai.expect(descriptors, "proxyData").to.include.keys("foo", "bar", "baz");
-        chai.expect(proxyData.foo, "proxyData.foo").to.equal(1);
-        chai.expect(proxyData.bar, "proxyData.bar").to.equal("bar");
-
-        const bazDescriptors = Object.getOwnPropertyDescriptors(proxyData.baz.constructor.prototype);
-
-        chai.expect(bazDescriptors, "proxyData.baz").to.include.keys("foobar");
-
-        chai.expect(proxyData.foo).to.equal(data.foo);
-        chai.expect(proxyData.bar).to.equal(data.bar);
-        chai.expect(proxyData.baz.foobar).to.equal(data.baz.foobar);
-
-        const foo    = data.foo;
-        const bar    = data.bar;
-        const foobar = data.baz.foobar;
-
-        proxyData.foo = 666;
-        proxyData.bar = "rab";
-        proxyData.baz.foobar = true;
-
-        chai.expect(foo,    "foo dont change").to.equal(data.foo);
-        chai.expect(bar,    "bar dont change").to.equal(data.bar);
-        chai.expect(foobar, "foobar dont change").to.equal(data.baz.foobar);
-
-        proxyData.undo();
-
-        chai.expect(proxyData.foo,        "proxyData.foo undone").to.equal(foo);
-        chai.expect(proxyData.bar,        "proxyData.bar undone").to.equal(bar);
-        chai.expect(proxyData.baz.foobar, "proxyData.baz.foobar undone").to.equal(foobar);
-
-        proxyData.foo = 666;
-        proxyData.bar = "rab";
-        proxyData.baz.foobar = true;
-
-        proxyData.save();
-
-        chai.expect(proxyData.foo,        "proxyData.foo save").to.equal(666);
-        chai.expect(proxyData.bar,        "proxyData.bar save").to.equal("rab");
-        chai.expect(proxyData.baz.foobar, "proxyData.baz.foobar save").to.equal(true);
-
-        const proxyData1 = new proxy(merge({ }, data));
-
-        proxyData1.baz.foobar = false;
-        proxyData1.save();
-        proxyData.undo();
-        chai.expect(proxyData.baz.foobar, "proxyData.baz.foobar save").to.equal(true);
     }
 }
