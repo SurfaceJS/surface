cb48dc1914c15de0ae113c5a32a8f5153e584657
Rafael França <hitalloexiled@hotmail.com>
2020-02-15 17:21:13 -0300

    refactoring: refactor CustomElement and removes unused funcionalities

--- source/@surface/custom-element/index.ts
+++ source/@surface/custom-element/index.ts
@@ -1,18 +1,22 @@
 import { Action, Indexer }                     from "@surface/core";
-import Reactive                                from "@surface/reactive";
 import ICustomElement                          from "./interfaces/custom-element";
 import References                              from "./internal/references";
 import { SCOPE, SHADOW_ROOT, STATIC_METADATA } from "./internal/symbols";
-import TemplateProcessor                       from "./internal/template-processor";
 import { StaticMetadata }                      from "./internal/types";
+
+type Scope = Indexer & { host?: HTMLElement };
 
 export default class CustomElement extends HTMLElement implements ICustomElement
 {
-    private [SCOPE]: Indexer = { };
-    private readonly [SHADOW_ROOT]: ShadowRoot;
-    private readonly _references:   References;
+    private readonly _references: References;
 
-    protected get context(): Indexer
+    protected readonly [SHADOW_ROOT]: ShadowRoot;
+
+    protected [SCOPE]: Scope = { };
+
+    public onAfterBind?: Action;
+
+    protected get scope(): Scope
     {
         return this[SCOPE];
     }
@@ -22,72 +26,33 @@ export default class CustomElement extends HTMLElement implements ICustomElement
         return this._references;
     }
 
-    public onAfterBind?: Action;
-
-    public constructor(shadowRootInit?: ShadowRootInit)
+    public constructor()
     {
         super();
 
-        this[SHADOW_ROOT] = this.attachShadow(shadowRootInit ?? { mode: "closed" });
+        const shadowRoot = this[SHADOW_ROOT] = this.attachShadow({ mode: "closed" });
 
+        this.applyMetadata(shadowRoot);
+
+        this._references = new References(shadowRoot);
+    }
+
+    private applyMetadata(shadowRoot: ShadowRoot): void
+    {
         const metadata = (this.constructor as Function & { [STATIC_METADATA]?: StaticMetadata })[STATIC_METADATA];
 
         if (metadata?.styles)
         {
-            (this[SHADOW_ROOT] as { adoptedStyleSheets?: Array<CSSStyleSheet> }).adoptedStyleSheets = metadata.styles ?? [];
+            (shadowRoot as { adoptedStyleSheets?: Array<CSSStyleSheet> }).adoptedStyleSheets = metadata.styles;
         }
 
         if (metadata?.template)
         {
-            this.applyTemplate(metadata.template);
+            const content = document.importNode(metadata.template.content, true);
+
+            content.normalize();
+
+            shadowRoot.appendChild(content);
         }
-
-        this._references = new References(this[SHADOW_ROOT]);
-    }
-
-    /**
-     * Process node tree directives
-     * @param content Node tree to be processed
-     * @param context Context utilized to resolve expressions
-     */
-    protected static processDirectives(host: Node, content: Node, context: Indexer): void
-    {
-        TemplateProcessor.process(host, content, context);
-    }
-
-    /**
-     * Remove binds reference of node tree.
-     * @param content Node tree to be unbinded
-     */
-    protected static clearDirectives(node: Node): void;
-    protected static clearDirectives(childNodes: NodeListOf<ChildNode>): void;
-    protected static clearDirectives(nodeOrChildNodes: Node|NodeListOf<ChildNode>): void
-    {
-        if (nodeOrChildNodes instanceof NodeList)
-        {
-            nodeOrChildNodes.forEach(x => TemplateProcessor.clear(x));
-        }
-        else
-        {
-            TemplateProcessor.clear(nodeOrChildNodes);
-        }
-    }
-
-    private applyTemplate(template: HTMLTemplateElement): void
-    {
-        const content = document.importNode(template.content, true);
-
-        content.normalize();
-
-        this[SHADOW_ROOT].appendChild(content);
-    }
-
-    /**
-     * Notify property change.
-     * @param key Property key
-     */
-    protected notify<K extends keyof this>(key: K)
-    {
-        Reactive.getReactor(this)?.notify(this, key);
     }
 }
