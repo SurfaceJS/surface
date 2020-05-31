9124b968dedf1ce646d01e53871a3b20aabc7e1e
Rafael França <hitalloexiled@hotmail.com>
2020-05-31 13:21:53 -0300

    refactor: minor changes

--- source/@surface/enumerable/internal/interfaces/lookup.d.ts
+++ source/@surface/enumerable/internal/interfaces/lookup.d.ts
@@ -1,4 +1,6 @@
-export default interface ILookup<TKey, TElement>
+import Group from "../group";
+
+export default interface ILookup<TKey, TElement> extends Iterable<Group<TKey, TElement>>
 {
     count:               number;
     contains(key: TKey): boolean
