730953717342a7288e2ac509197e1eb927c94d81
Rafael França <hitalloexiled@hotmail.com>
2020-09-27 12:10:10 -0300

    fix: Fixes commands descriptions.

--- source/@surface/compiler/internal/bin/build.ts
+++ source/@surface/compiler/internal/bin/build.ts
@@ -15,7 +15,7 @@ const program = createProgram()
     .option("--hot               [n]", "Enable hot module reload.", toBoolean)
     .option("--log-level         <n>", "Output verbosity level. Can be 'errors-only', 'minimal', 'none', 'normal', 'verbose'.", parsePattern(logLevelPattern))
     .option("--mode              <n>", "Enable production optimizations or development hints.", parsePattern(modePattern))
-    .option("--watch             [n]", "Enable Watch mode compilation", toBoolean)
+    .option("--watch             [n]", "Enable Watch mode compilation.", toBoolean)
     .action((command: Command) => void Tasks.build(command.opts()));
 
 program.parse(process.argv);
