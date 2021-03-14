15a8932bf13220b046b0208fbfde40e628fd8b2a
Rafael França <hitalloexiled@hotmail.com>
2021-03-14 14:37:07 -0300

    fix: Fixes "ENOENT: no such file or directory, chmod '/home/runner/work/modules/modules/node_modules/@surface/cli/cli.js'" error.

---
+++ packages/@surface/cli/cli.js
@@ -0,0 +1,9 @@
+import commander from "commander";
+import Tasks from "./internal/tasks.js";
+const program = new commander.Command();
+program.version("1.0.0");
+program
+    .command("new")
+    .action(Tasks.new);
+program.parse(process.argv);
+//# sourceMappingURL=cli.js.map
