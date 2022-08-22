c8f7675a30347e5cb5b6ad1ca3cca53dd66b6560
Rafael França <rafael.franca@meta.com.br>
2022-08-22 10:03:11 -0300

    fix: fixes build for release

--- packages/path-matcher/index.ts
+++ packages/path-matcher/index.ts
@@ -1,6 +1,6 @@
 import { type Options }                      from "./internal/parser.js";
 import PathMatcher, { type ResolvedPattern } from "./internal/path-matcher.js";
 
-export { Options, ResolvedPattern };
+export type { Options, ResolvedPattern };
 
 export default PathMatcher;
