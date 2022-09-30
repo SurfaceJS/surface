39ccef594174cbe66917c41548cabb9c006149c3
Rafael França <hitalloexiled@hotmail.com>
2022-09-30 14:37:48 -0300

    fix(htmlx): fixes htmlx directive factory

--- packages/htmlx/internal/factories/create-directive-factory.ts
+++ packages/htmlx/internal/factories/create-directive-factory.ts
@@ -1,11 +1,11 @@
-import type { IDisposable }                from "@surface/core";
-import { typeGuard }                       from "@surface/core";
-import type { ObservablePath, StackTrace } from "@surface/htmlx-parser";
-import { buildStackTrace }                 from "../common.js";
-import TemplateProcessError                from "../errors/template-process-error.js";
-import type AttributeFactory               from "../types/attribute-factory.js";
-import type DirectiveContext               from "../types/directive-context.js";
-import type Evaluator                      from "../types/evaluator.js";
+import { CancellationTokenSource, type IDisposable, typeGuard } from "@surface/core";
+import type { ObservablePath, StackTrace }                      from "@surface/htmlx-parser";
+import { buildStackTrace }                                      from "../common.js";
+import TemplateProcessError                                     from "../errors/template-process-error.js";
+import { scheduler }                                            from "../singletons.js";
+import type AttributeFactory                                    from "../types/attribute-factory.js";
+import type DirectiveContext                                    from "../types/directive-context.js";
+import type Evaluator                                           from "../types/evaluator.js";
 
 type DirectiveFactory = (context: DirectiveContext) => IDisposable;
 
@@ -13,33 +13,40 @@ export default function createDirectiveFactory(key: string, evaluator: Evaluator
 {
     return (element, scope, directives) =>
     {
-        const disposables: IDisposable[] = [];
-        const handlerConstructor = directives.get(key);
+        const cancellation = new CancellationTokenSource();
+        const disposables: IDisposable[] = [{ dispose: () => cancellation.cancel() }];
 
-        if (!handlerConstructor)
+        const action = (): void =>
         {
-            throw new TemplateProcessError(`Unregistered directive #${key}.`, buildStackTrace(stackTrace ?? []));
-        }
+            const handlerConstructor = directives.get(key);
 
-        const context: DirectiveContext =
-        {
-            element,
-            key,
-            observables,
-            scope,
-            source,
-            stackTrace,
-            value: evaluator,
+            if (!handlerConstructor)
+            {
+                throw new TemplateProcessError(`Unregistered directive #${key}.`, buildStackTrace(stackTrace ?? []));
+            }
+
+            const context: DirectiveContext =
+            {
+                element,
+                key,
+                observables,
+                scope,
+                source,
+                stackTrace,
+                value: evaluator,
+            };
+
+            if (typeGuard<DirectiveFactory>(handlerConstructor, !handlerConstructor.prototype))
+            {
+                disposables.push(handlerConstructor(context));
+            }
+            else
+            {
+                disposables.push(new handlerConstructor(context));
+            }
         };
 
-        if (typeGuard<DirectiveFactory>(handlerConstructor, !handlerConstructor.prototype))
-        {
-            disposables.push(handlerConstructor(context));
-        }
-        else
-        {
-            disposables.push(new handlerConstructor(context));
-        }
+        void scheduler.enqueue(action, "high", cancellation.token);
 
         return { dispose: () => disposables.splice(0).forEach(x => x.dispose()) };
     };
